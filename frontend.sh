8fe239988913e66ab3d5e677e84ba2a70f035447
vinith <vinithamm007@gmail.com>
2023-08-30 20:25:24 +0530

    frontend

--- frontend.sh
+++ frontend.sh
@@ -1,6 +1,17 @@
-a
-b
-c
-declare
-echo
-amma vinith
+
+yum install nginx -y
+
+cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+systemctl enable nginx
+systemctl start nginx
+
+rm -rf /usr/share/nginx/html/*
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+cd /usr/share/nginx/html
+
+unzip /tmp/frontend.zip
+
+systemctl restart nginx
