919bfcd285c2d01ed0b58986acd15fc06b0fbf27
vinith <vinithamm007@gmail.com>
2023-08-30 22:05:46 +0530

    roboshop scripts

---
+++ catalogue.sh
@@ -0,0 +1,19 @@
+cp catalogue.service /etc/systemd/system/catalogue.service
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+mkdir /app
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+cd /app
+unzip /tmp/catalogue.zip
+cd /app
+npm install
+yum install mongodb-org-shell -y
+mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
+# update mongo ip address
+# update catalogue server ip address in frontend configuration
+systemctl daemon reload
+systemctl enable catalogue
+systemctl restart catalogue
+
--- mongodb.sh
+++ mongodb.sh
@@ -1,2 +1,7 @@
-hi
-this is vinith
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+yum install mongodb-org -y
+# update listen address 127.0.0.1 to 0.0.0.0
+systemctl enable mongod
+systemctl start mongod
+
