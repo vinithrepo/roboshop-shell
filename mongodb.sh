347e5c2de86c2a02ee71ddaedf249c2abe730c60
Vinith Amma <143014366+vinithrepo@users.noreply.github.com>
2023-09-01 22:46:14 +0530

    minor changes

--- mongodb.sh
+++ mongodb.sh
@@ -3,5 +3,5 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
 # update listen address 127.0.0.1 to 0.0.0.0
 systemctl enable mongod
-systemctl start mongod
+systemctl restart mongod
 
