21ce9d2e45b6cd101b09de0801673f2b306c121c
Vinith Amma <143014366+vinithrepo@users.noreply.github.com>
2023-09-01 22:34:42 +0530

    minor changes

--- cart.sh
+++ cart.sh
@@ -3,11 +3,11 @@ curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 useradd roboshop
 mkdir /app
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app
 unzip /tmp/cart.zip
 cd /app
 npm install
 systemctl daemon-reload
 systemctl enable cart
-systemctl start cart
+systemctl restart cart
