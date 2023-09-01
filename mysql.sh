9656097858619554d2af3ea68b843034ddcb02d0
Vinith Amma <143014366+vinithrepo@users.noreply.github.com>
2023-09-01 22:48:15 +0530

    minor changes

--- mysql.sh
+++ mysql.sh
@@ -2,7 +2,6 @@ cp mysql.repo /etc/yum.repos.d/mysql.repo
 yum module disable mysql -y
 yum install mysql-community-server -y
 systemctl enable mysqld
-systemctl start mysqld
+systemctl restart mysqld
 mysql_secure_installation --set-root-pass RoboShop@1
-mysql -uroot -pRoboShop@1
 
