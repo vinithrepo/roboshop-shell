b965f41a216c0dc3171c8138dfdb7e5a9a4d577f
Vinith Amma <143014366+vinithrepo@users.noreply.github.com>
2023-09-01 22:40:54 +0530

    rm updated

--- catalogue.sh
+++ catalogue.sh
@@ -3,6 +3,7 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 useradd roboshop
+rm -rf /app
 mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
@@ -13,7 +14,7 @@ yum install mongodb-org-shell -y
 mongo --host mongodb.akhildevops.online </app/schema/catalogue.js
 # update mongo ip address
 # update catalogue server ip address in frontend configuration
-systemctl daemon reload
+systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
 
