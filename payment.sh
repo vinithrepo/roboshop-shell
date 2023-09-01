455d8bbde2842424e7812594d40fb33ebce7f6ef
Vinith Amma <143014366+vinithrepo@users.noreply.github.com>
2023-09-02 00:25:11 +0530

    minor change

--- payment.sh
+++ payment.sh
@@ -1,4 +1,4 @@
-cp payment.service /etc/systemdsystem/payment.service
+cp payment.service /etc/systemd/system/payment.service
 yum install python36 gcc python3-devel -y
 useradd roboshop
 rm -rf /app
