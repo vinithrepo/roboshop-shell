a42a4b6f70947dc3f0dba11e37e6cf1cab339323
vinith <vinithamm007@gmail.com>
2023-09-03 16:46:03 +0530

    listen address updated

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,7 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
 yum install mongodb-org -y
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 # update listen address 127.0.0.1 to 0.0.0.0
 systemctl enable mongod
 systemctl restart mongod
