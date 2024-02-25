90d3258b277144e3fc8242dcc88222f29df68a03
vinith <vinithamm007@gmail.com>
2024-02-25 13:18:12 +0530

    dispacth lines

--- dispatch.sh
+++ dispatch.sh
@@ -6,11 +6,13 @@ useradd roboshop
 mkdir /app
 
 curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
+
 cd /app
 
 unzip /tmp/dispatch.zip
 
 cd /app
+
 go mod init dispatch
 go get
 go build
