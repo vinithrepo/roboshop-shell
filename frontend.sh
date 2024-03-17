5b5a1e2cc4dadc6e32e5858dfd258daeae77fb58
vinith <vinithamm007@gmail.com>
2024-03-17 14:21:08 +0530

    using yum module

--- frontend.sh
+++ frontend.sh
@@ -1,7 +1,8 @@
+
+
+yum install nginx -y
+
 cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
-
-dnf install nginx -y
-
 rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
