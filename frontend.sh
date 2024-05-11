c4182495c224fe4d8483a41ed652d0271e6c2dab
vinith <vinithamm007@gmail.com>
2024-05-11 17:43:42 +0530

    frontend 2.o

--- frontend.sh
+++ frontend.sh
@@ -1,14 +1,13 @@
 
+dnf install nginx -y
 
-yum install nginx -y
+cp nginx-roboshop.conf  /etc/nginx/default.d/roboshop.conf
 
-cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
 cd /usr/share/nginx/html
-
 unzip /tmp/frontend.zip
 
 systemctl enable nginx
