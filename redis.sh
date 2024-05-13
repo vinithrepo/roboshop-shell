f9ccee8010907d5e218579338f4191da0bc87b8e
vinith <vinithamm007@gmail.com>
2024-05-13 21:49:08 +0530

    dnf module

--- redis.sh
+++ redis.sh
@@ -1,8 +1,8 @@
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 
-yum module enable redis:remi-6.2 -y
+dnf module enable redis:remi-6.2 -y
 
-yum install redis -y
+dnf install redis -y
 
 #Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/redis.conf & /etc/redis/redis.conf
 
