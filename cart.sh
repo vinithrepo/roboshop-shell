32185513962027eee8c210b1b5b6ddb9b875d359
vinith <vinithamm007@gmail.com>
2024-05-14 20:44:01 +0530

    dnf module using for cart

--- cart.sh
+++ cart.sh
@@ -1,15 +1,15 @@
-cp cart.service /etc/systemd/system/cart.service
+cp cart.service  /etc/systemd/system/cart.service
 
-yum module disable nodejs -y
-yum module enable nodejs:18 -y
+dnf module disable nodejs -y
+dnf module enable nodejs:18 -y
 
-yum install nodejs -y
+dnf install nodejs -y
 
 useradd roboshop
 
 mkdir /app
 
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app
 unzip /tmp/cart.zip
 
@@ -19,4 +19,5 @@ npm install
 systemctl daemon-reload
 
 systemctl enable cart
-systemctl restart cart
+systemctl start cart
+
