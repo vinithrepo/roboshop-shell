5af04719208ece63a593531daa12719123687c94
vinith <vinithamm007@gmail.com>
2024-05-21 00:20:44 +0530

    adding colors to messages

--- catalogue.sh
+++ catalogue.sh
@@ -1,37 +1,37 @@
 
-echo ">>>> creating catalogue service  <<<<"
+echo  -e "\e[32m>>>> creating catalogue service  <<<<\e[0m"
 cp catalogue.service /etc/systemd/system/catalogue.service
-echo ">>>> mongo repo <<<<"
+echo  -e "\e[32m>>>> mongo repo <<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-echo ">>>> disable nodejs <<<<"
+echo  -e "\e[32m>>>> disable nodejs <<<<\e[0m"
 dnf module disable nodejs -y
-echo ">>>> enable nodejs`` <<<<"
+echo  -e "\e[32m>>>> enable nodejs`` <<<<\e[0m"
 dnf module enable nodejs:18 -y
-echo ">>>> install nodejs <<<<"
+echo  -e "\e[32m>>>> install nodejs <<<<\e[0m"
 dnf install nodejs -y
-echo ">>>> adding user  <<<<"
+echo  -e "\e[32m>>>> adding user  <<<<\e[0m"
 useradd roboshop
-echo ">>>> cleaning directory  <<<<"
+echo  -e "\e[32m>>>> cleaning directory  <<<<\e[0m"
 rm -rf /app
 
 mkdir /app
-echo ">>>> dowlnloading zip <<<<"
+echo  -e "\e[32m>>>> dowlnloading zip <<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-echo ">>> extracting zip <<<"
+echo  -e "\e[32m>>> extracting zip <<<\e[0m"
 cd /app
 unzip /tmp/catalogue.zip
 
 cd /app
-echo ">>>> dowlnloading dependencies <<<<"
+echo  -e "\e[32m>>>> dowlnloading dependencies <<<<\e[0m"
 npm install
-echo ">>>> install DB shell <<<<"
+echo  -e "\e[32m>>>> install DB shell <<<<\e[0m"
 dnf install mongodb-org-shell -y
 
-echo ">>>> load schema <<<<"
+echo  -e "\e[32m>>>> load schema <<<<\e[0m"
 
 mongo --host mongodb.vinithaws.online </app/schema/catalogue.js
 
-echo ">>>> start service <<<<"
+echo  -e "\e[32m>>>> start service <<<<\e[0m"
 
 systemctl daemon-reload
 
