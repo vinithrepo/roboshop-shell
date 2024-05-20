a5ee7cd3f0d60bbf6231d4f94537284fe53e4a85
vinith <vinithamm007@gmail.com>
2024-05-21 00:29:12 +0530

    using variable

--- catalogue.sh
+++ catalogue.sh
@@ -1,35 +1,36 @@
+log=/tmp/roboshop.log
 
 echo  -e "\e[32m>>>> creating catalogue service  <<<<\e[0m"
-cp catalogue.service /etc/systemd/system/catalogue.service  &>>/tmp/roboshop.log
+cp catalogue.service /etc/systemd/system/catalogue.service  &>>{log}
 echo  -e "\e[32m>>>> mongo repo <<<<\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo   &>>/tmp/roboshop.log
+cp mongo.repo /etc/yum.repos.d/mongo.repo   &>>{log}
 echo  -e "\e[32m>>>> disable nodejs <<<<\e[0m"
-dnf module disable nodejs -y   &>>/tmp/roboshop.log
+dnf module disable nodejs -y   &>>{log}
 echo  -e "\e[32m>>>> enable nodejs`` <<<<\e[0m"
-dnf module enable nodejs:18 -y    &>>/tmp/roboshop.log
+dnf module enable nodejs:18 -y    &>>{log}
 echo  -e "\e[32m>>>> install nodejs <<<<\e[0m"
-dnf install nodejs -y   &>>/tmp/roboshop.log
+dnf install nodejs -y   &>>{log}
 echo  -e "\e[32m>>>> adding user  <<<<\e[0m"
-useradd roboshop   &>>/tmp/roboshop.log
+useradd roboshop   &>>{log}
 echo  -e "\e[32m>>>> cleaning directory  <<<<\e[0m"
-rm -rf /app  &>>/tmp/roboshop.log
+rm -rf /app  &>>{log}
 
 mkdir /app
 echo  -e "\e[32m>>>> dowlnloading zip <<<<\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip   &>>/tmp/roboshop.log
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip   &>>{log}
 echo  -e "\e[32m>>> extracting zip <<<\e[0m"
 cd /app
-unzip /tmp/catalogue.zip  &>>/tmp/roboshop.log
+unzip /tmp/catalogue.zip  &>>{log}
 
 cd /app
 echo  -e "\e[32m>>>> dowlnloading dependencies <<<<\e[0m"
-npm install   &>>/tmp/roboshop.log
+npm install   &>>{log}
 echo  -e "\e[32m>>>> install DB shell <<<<\e[0m"
-dnf install mongodb-org-shell -y   &>>/tmp/roboshop.log
+dnf install mongodb-org-shell -y   &>>{log}
 
 echo  -e "\e[32m>>>> load schema <<<<\e[0m"
 
-mongo --host mongodb.vinithaws.online </app/schema/catalogue.js   &>>/tmp/roboshop.log
+mongo --host mongodb.vinithaws.online </app/schema/catalogue.js   &>>{log}
 
 echo  -e "\e[32m>>>> start service <<<<\e[0m"
 
