afb2ef6c905cfe0b7930d37d366f5f068b4d4ef6
vinith <vinithamm007@gmail.com>
2024-06-09 15:10:50 +0530

    password

--- mysql.sh
+++ mysql.sh
@@ -1,11 +1,14 @@
 mysql_root_password=$1
+
 cp mysql.repo  /etc/yum.repos.d/mysql.repo
 
 dnf module disable mysql -y
 
 dnf install mysql-community-server -y
 
-mysql_secure_installation --set-root-pass ${mysql_root_password}
-
 systemctl enable mysqld
 systemctl restart mysqld
+
+mysql_secure_installation --set-root-pass ${mysql_root_password}
+
+
